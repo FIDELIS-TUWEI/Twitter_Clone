e2812cb31eda1715aeb472f86476a46c6dfa3d8e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-02-20 05:45:20 +0300

    routes created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,10 @@ const Home = () => {
 
     return ( 
         <>
-            <h1>Home Page</h1>
+            <div>
+                <h2>Home Page</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores rerum, deserunt voluptatem debitis est cum fugit expedita? A obcaecati voluptate dignissimos delectus suscipit sed repellat asperiores quam. Neque, expedita eius.</p>
+            </div>
         </>
      );
 }
