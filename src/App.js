ce393fa7a602fa23654c9da33850e0fc52108a90
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-04 14:39:44 +0300

    folder cleanup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
-import { 
-  createBrowserRouter, 
-  Route, 
-  createRoutesFromElements,
-  RouterProvider
-} from 'react-router-dom';
 
-// pages
-import Home from './pages/Home'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import Products from './pages/Products'
-
-// layout
-import RootLayout from './layouts/RootLayout';
-
-// createbrowserrouter 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<RootLayout />}>
-      <Route index element={<Home />} />
-      <Route path='login' element={<Login />} />
-      <Route path='signup' element={<SignUp />} />
-      <Route path='products' element={<Products />} />
-    </Route>
-  )
-)
 
 function App() {
 
   return (
-      
-      <RouterProvider router={router}/>
+      <></>
   )
 }
 
