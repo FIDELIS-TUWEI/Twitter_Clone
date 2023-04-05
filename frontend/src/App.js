26f45210cacb8b8a9a41f33aabae2c70f8c83299
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-05 09:35:39 +0300

    Routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import POSPage from "./pages/POSPage";
 
 function App() {
 
   return (
-      <></>
+      <div className="App">
+        <Routes>
+          <Route path="/" index element={<HomePage />} />
+          <Route path="/pos" element={<POSPage />} />
+        </Routes>
+      </div>
   )
 }
 
