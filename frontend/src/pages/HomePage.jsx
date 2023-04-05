6dcd232ff085b4e7b8c9d01496975ea03cd67851
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-05 09:57:56 +0300

    content added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,14 +4,23 @@ const HomePage = () => {
     return ( 
         <div>
             <header>
-                <nav className="navbar navbar-light bg-light">
-                    <div className="container-fluid">
+                <nav className="navbar navbar-light bg-primary">
+                    <div className="container">
                         <Link to="/" className="navbar-brand">DevPOS</Link>
                     </div>
                 </nav>
             </header>
 
-            <main></main>
+            <main>
+                <div className="container mt-3">
+                    <div className="bg-light pt-5 mt-4 rounded-3">
+                        <h1>Welcome to the simple POS for small business</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, nobis!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                        <Link to='/pos' className='btn btn-primary'>Click here to sell products</Link>
+                    </div>
+                </div>
+            </main>
         </div>
      );
 }
