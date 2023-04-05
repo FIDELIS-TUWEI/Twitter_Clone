5610e0a8f1b925561cc0f2dcf2ac18fe9ad4e4e4
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-05 10:31:58 +0300

    fetching data with axios

--- frontend/src/pages/POSPage.jsx
+++ frontend/src/pages/POSPage.jsx
@@ -1,12 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import MainLayout from "../layouts/MainLayout"
+import axios from 'axios'
 
 const POSPage = () => {
 
+    // useState
+    const [products, setProducts] = useState([])
+
+    // function to fetch products
+    const fetchProducts = async() => {
+        const result = await axios.get("products");
+        setProducts(await result.data);
+
+    }
+
     // get data from backend
     useEffect(() => {
-        
-    },[]);
+        fetchProducts();
+    }, []);
+
+    // useEffect for products
+    useEffect(() => {
+        console.log(products)
+    }, [products])
 
     return ( 
         <MainLayout>
