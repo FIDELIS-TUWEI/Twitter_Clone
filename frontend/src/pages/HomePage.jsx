526fb526eb251e70f1af074f5933879d9f4cebe1
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-05 09:48:31 +0300

    navbar created

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,18 @@
+import Link  from 'react-router-dom'
+
 const HomePage = () => {
     return ( 
-        <></>
+        <div>
+            <header>
+                <nav className="navbar navbar-light bg-light">
+                    <div className="container-fluid">
+                        <Link to="/" className="navbar-brand">DevPOS</Link>
+                    </div>
+                </nav>
+            </header>
+
+            <main></main>
+        </div>
      );
 }
  
