7b53f74a8fd77f85288fe7141e576bd9afa9ee44
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-05 10:06:11 +0300

    Homepage changes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,27 +1,16 @@
 import {Link}  from 'react-router-dom'
+import MainLayout from '../layouts/MainLayout';
 
 const HomePage = () => {
     return ( 
-        <div>
-            <header>
-                <nav className="navbar navbar-light bg-primary">
-                    <div className="container">
-                        <Link to="/" className="navbar-brand">DevPOS</Link>
-                    </div>
-                </nav>
-            </header>
-
-            <main>
-                <div className="container mt-3">
-                    <div className="bg-light pt-5 mt-4 rounded-3">
-                        <h1>Welcome to the simple POS for small business</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, nobis!</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                        <Link to='/pos' className='btn btn-primary'>Click here to sell products</Link>
-                    </div>
-                </div>
-            </main>
-        </div>
+        <MainLayout>
+            <div className="bg-light pt-5 mt-4 rounded-3">
+                <h1>Welcome to the simple POS for small business</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, nobis!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <Link to='/pos' className='btn btn-primary'>Click here to sell products</Link>
+            </div>
+        </MainLayout>
      );
 }
  
