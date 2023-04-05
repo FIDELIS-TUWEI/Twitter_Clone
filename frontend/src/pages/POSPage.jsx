519c93cab62abaa5845e22cd5310f49c22a60bb7
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-05 18:47:32 +0300

    product mapped

--- frontend/src/pages/POSPage.jsx
+++ frontend/src/pages/POSPage.jsx
@@ -5,18 +5,19 @@ import axios from 'axios'
 const POSPage = () => {
 
     // useState
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     // function to fetch products
     const fetchProducts = async() => {
         try {
+            setIsLoading(true);
             const result = await axios.get("products");
             setProducts(await result.data);
+            setIsLoading(false);
         } catch (error) {
             console.log(error)
         }
-        
-
     }
 
     // get data from backend
@@ -31,7 +32,22 @@ const POSPage = () => {
 
     return ( 
         <MainLayout>
-            POS content
+            <div className="row">
+                <div className="col-lg-8">
+                    { isLoading ? 'Loading' : <div className="row">
+                        {products.map((product, key) => {
+                            <div key={key} className="col-lg-4">
+                                <div className="border">
+                                    <p>{product.name}</p>
+                                    <img src={product.image} className="img-fluid" alt={product.name} />
+                                    <p>${product.price}</p>
+                                </div>
+                            </div>
+                        })}
+                    </div>}
+                    
+                </div>
+            </div>
         </MainLayout>
      );
 }
