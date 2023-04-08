e580fe88b0ea8f903b9b05795057f9a6a2b37fe3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-08 16:33:46 +0300

    CircularProgress import

--- frontend/src/pages/POSPage.jsx
+++ frontend/src/pages/POSPage.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { toast } from "react-toastify";
 import { ComponentToPrint } from "../components/ComponentToPrint";
 import { useReactToPrint } from 'react-to-print';
+import {CircularProgress} from '@mui/material'
 
 const POSPage = () => {
 
