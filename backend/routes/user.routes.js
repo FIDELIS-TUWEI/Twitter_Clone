067633e1df29d34896bf67e1562b3a7374b54cf6
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-02 13:16:46 +0300

    route to get suggested users

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { protectRoute } = require("../middleware/protectRoute");
-const { getUserProfile, followUnfollowUser } = require("../controllers/user.controller");
+const { getUserProfile, followUnfollowUser, getSuggestedUsers } = require("../controllers/user.controller");
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
-//router.get("/suggested", protectRoute, getSuggestedUsers);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
 //router.post("/update", protectRoute, updateUserProfile);
 
