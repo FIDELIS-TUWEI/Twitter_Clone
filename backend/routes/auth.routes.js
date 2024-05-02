1c77826467b139d77346d42b59d42907cfe2ae49
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-02 10:05:56 +0300

    get me route added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { signup, login, logout } = require("../controllers/auth.controller");
+const { signup, login, logout, getMe } = require("../controllers/auth.controller");
+const { protectRoute } = require("../middleware/protectRoute");
 const router = express.Router();
 
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
-
 router.post("/login", login);
-
-router.get("/logout", logout);
+router.post("/logout", logout);
 
 module.exports = router;
