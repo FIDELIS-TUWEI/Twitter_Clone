058fabcec650e2ef03a13c93b63e09401f02a2aa
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-02 09:19:35 +0300

    route modified for post request

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { signup, login, logout } = require("../controllers/auth.controller");
 const router = express.Router();
 
-router.get("/signup", signup);
+router.post("/signup", signup);
 
 router.get("/login", login);
 
