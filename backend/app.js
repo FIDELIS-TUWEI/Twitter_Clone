e66cd9865d424d4018e5eb402097c479d3d2c5df
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-02 10:05:11 +0300

    cookie-parser middleware added

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 const authRoutes = require("./routes/auth.routes");
 
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(cookieParser());
 app.use(cors());
 app.disable("x-powered-by");
 
