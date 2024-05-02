99decaf53e27982b079cb98175bc4e7f6a539ccb
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-02 09:18:50 +0300

    additional environment variables set

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 let PORT = process.env.PORT;
 let MONGODB_URI = process.env.MONGODB_URI;
+let JWT_SECRET = process.env.JWT_SECRET;
+let NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
-    PORT, MONGODB_URI
+    PORT, MONGODB_URI, JWT_SECRET, NODE_ENV
 };
