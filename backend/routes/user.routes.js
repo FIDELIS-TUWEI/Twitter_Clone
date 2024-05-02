ec09ec9b033ade765fbdda633aad03895aa47871
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-02 12:38:55 +0300

    route to follow/ unfollow user

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { protectRoute } = require("../middleware/protectRoute");
-const { getUserProfile } = require("../controllers/user.controller");
+const { getUserProfile, followUnfollowUser } = require("../controllers/user.controller");
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
 //router.get("/suggested", protectRoute, getSuggestedUsers);
-//router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
 //router.post("/update", protectRoute, updateUserProfile);
 
 module.exports = router;
