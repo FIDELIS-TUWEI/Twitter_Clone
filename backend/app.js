ab35f1b95822f41da3c02ff92f17218e35486695
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-02 09:19:08 +0300

    app middleware to parse urlencoded

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const app = express();
 const authRoutes = require("./routes/auth.routes");
 
-app.use(express.json());
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 app.use(cors());
 app.disable("x-powered-by");
 
