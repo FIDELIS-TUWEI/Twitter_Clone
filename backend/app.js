a35f464f514b13c05810e03c9dc988e9705bbb53
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-02 07:56:24 +0300

    express.json middleware added

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const authRoutes = require("./routes/auth.routes");
 
-
+app.use(express.json());
+app.use(cors());
 app.disable("x-powered-by");
 
 app.get("/", (req, res) => {
