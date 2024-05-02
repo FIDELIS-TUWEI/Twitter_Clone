d6b4df9f0f544ef4ff78bbfe1230dd844b603080
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-02 12:02:16 +0300

    users route imported

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
 const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
 
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
@@ -15,5 +16,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 module.exports = app;
