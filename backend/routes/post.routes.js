58e8f8ebaf0678372952b10af1ad0a49ff3b20fb
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-03 15:42:47 +0300

    delete post route

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { protectRoute } = require("../middleware/protectRoute");
-const { createPost } = require("../controllers/post.controller");
+const { createPost, deletePost } = require("../controllers/post.controller");
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
+router.delete("/delete/:id", protectRoute, deletePost);
 
 module.exports = router;
