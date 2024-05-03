478aa9580789b21923781843fed0a04552fd3308
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-03 15:57:54 +0300

    comment on post route added

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { protectRoute } = require("../middleware/protectRoute");
-const { createPost, deletePost } = require("../controllers/post.controller");
+const { createPost, deletePost, commentOnPost } = require("../controllers/post.controller");
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
+router.post("/comment/:id", protectRoute, commentOnPost);
 router.delete("/delete/:id", protectRoute, deletePost);
 
 module.exports = router;
