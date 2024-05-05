95f6000d91d2c9f4271c530019c15c275c774c88
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-05 10:05:11 +0300

    assets import in one file

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,19 @@
+import boy1 from "./avatars/boy1.png";
+import boy2 from "./avatars/boy2.png";
+import boy3 from "./avatars/boy3.png";
+import girl1 from "./avatars/girl1.png";
+import girl2 from "./avatars/girl2.png";
+import girl3 from "./avatars/girl3.png";
+
+import post1 from "./posts/post1.png";
+import post2 from "./posts/post2.png";
+import post3 from "./posts/post3.png";
+
+import avatarPlaceholder from "./avatar-placeholder.png";
+import cover from "./cover.png";
+
+export {
+    boy1, boy2, boy3, girl1, girl2, girl3,
+    post1, post2, post3,
+    avatarPlaceholder, cover
+}
