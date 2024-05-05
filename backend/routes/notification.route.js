b2742d6e913dc668a4d9b0aadaa913a407f7ee35
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-05 09:51:30 +0300

    route to delete single notification

--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { protectRoute } = require("../middleware/protectRoute");
-const { getNotifications, deleteNotifications } = require("../controllers/notification.controller");
+const { getNotifications, deleteNotifications, deleteNotification } = require("../controllers/notification.controller");
 const router = express.Router();
 
 router.get("/", protectRoute, getNotifications);
 router.delete("/", protectRoute, deleteNotifications);
+router.delete("/:id", protectRoute, deleteNotification);
 
 module.exports = router;
