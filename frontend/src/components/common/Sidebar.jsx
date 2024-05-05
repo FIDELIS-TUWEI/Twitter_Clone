a42771d016c21485a7ce81c23e4494cbf19a982e
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-05 17:49:16 +0300

    logic to invalidate user when logged out

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -1,14 +1,15 @@
 import XSvg from "../svgs/X";
-import { boy1 } from "../../assets/index";
+import { avatarPlaceholder } from "../../assets/index";
 import { MdHomeFilled } from "react-icons/md";
 import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 const Sidebar = () => {
+	const queryClient = useQueryClient();
 
 	const { mutate: logOutMutation } = useMutation({
 		mutationFn: async () => {
@@ -25,18 +26,14 @@ const Sidebar = () => {
 			}
 		},
 		onSuccess: () => {
-			toast.success("Logged out successfully");
+			queryClient.invalidateQueries({ queryKey: ["authUser"] });
 		},
 		onError: () => {
 			toast.error("Failed to logout");
 		}
 	})
 
-	const data = {
-		fullName: "John Doe",
-		username: "johndoe",
-		profileImg: boy1,
-	};
+	const { data: authUser } = useQuery({ queryKey: ["authUser"] });
 
 	return (
 		<div className='md:flex-[2_2_0] w-18 max-w-52'>
@@ -66,7 +63,7 @@ const Sidebar = () => {
 
 					<li className='flex justify-center md:justify-start'>
 						<Link
-							to={`/profile/${data?.username}`}
+							to={`/profile/${authUser?.username}`}
 							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
 						>
 							<FaUser className='w-6 h-6' />
@@ -74,20 +71,20 @@ const Sidebar = () => {
 						</Link>
 					</li>
 				</ul>
-				{data && (
+				{authUser && (
 					<Link
-						to={`/profile/${data.username}`}
+						to={`/profile/${authUser.username}`}
 						className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'
 					>
 						<div className='avatar hidden md:inline-flex'>
 							<div className='w-8 rounded-full'>
-								<img src={data?.profileImg || "/avatar-placeholder.png"} alt="profileImg" />
+								<img src={authUser?.profileImg || avatarPlaceholder} alt="profileImg" />
 							</div>
 						</div>
 						<div className='flex justify-between flex-1'>
 							<div className='hidden md:block'>
-								<p className='text-white font-bold text-sm w-20 truncate'>{data?.fullName}</p>
-								<p className='text-slate-500 text-sm'>@{data?.username}</p>
+								<p className='text-white font-bold text-sm w-20 truncate'>{authUser?.fullName}</p>
+								<p className='text-slate-500 text-sm'>@{authUser?.username}</p>
 							</div>
 							<BiLogOut className='w-5 h-5 cursor-pointer' 
 								onClick={(e) => {
