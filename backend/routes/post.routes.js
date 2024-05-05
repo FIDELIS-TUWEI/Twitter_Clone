3e2f93a8ec91ca07b44116909c43a55823f101b0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-05-05 06:52:18 +0300

    like/unlike post route

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { protectRoute } = require("../middleware/protectRoute");
-const { createPost, deletePost, commentOnPost } = require("../controllers/post.controller");
+const { createPost, deletePost, commentOnPost, likeUnlikePost } = require("../controllers/post.controller");
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
 router.delete("/delete/:id", protectRoute, deletePost);
 
