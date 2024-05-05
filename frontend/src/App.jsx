10875e12f8a6f12ff672581233ec69bb80af0c04
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-05 11:48:23 +0300

    frontend UI design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import { HomePage, LoginPage, NotificationPage, ProfilePage, SignUpPage } from "./pages";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+
+
 const App = () => {
   return (
-    <div className="text-2xl font-bold text-center">
-      App
-    </div>
+    <div className='flex max-w-6xl mx-auto'>
+		<Sidebar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/notifications' element={<NotificationPage />} />
+				<Route path='/profile/:username' element={<ProfilePage />} />
+
+			</Routes>
+			<RightPanel />
+		</div>
   )
 };
 
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,9 @@
+import HomePage from "./home/HomePage";
+import SignUpPage from "./auth/signup/SignUpPage";
+import LoginPage from "./auth/login/LoginPage";
+import NotificationPage from "./notification/NotificationPage";
+import ProfilePage from "./profile/ProfilePage";
+
+export {
+    HomePage, SignUpPage, LoginPage, NotificationPage, ProfilePage
+};
