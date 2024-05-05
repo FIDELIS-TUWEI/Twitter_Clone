634523a0e3da05589f1c0582298fda06ae232424
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-05 09:51:13 +0300

    controller logic to delete single notification

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -15,7 +15,7 @@ const getNotifications = async (req, res) => {
         await Notification.updateMany({ to: userId }, { read: true });
 
         res.status(200).json(notifications);
-        
+
     } catch (error) {
         logger.error("Error in getNotifications controller", error);
         res.status(500).json({ error: "Internal Server Error" });
@@ -37,6 +37,26 @@ const deleteNotifications = async (req, res) => {
     }
 };
 
+const deleteNotification = async (req, res) => {
+    try {
+        const notificationId = req.params.id;
+        const userId = req.user._id;
+        const notification = await Notification.findById(notificationId);
+
+        if (!notification) return res.status(404).json({ error: "Notification not found" });
+
+        if (notification.to.toString() !== userId.toString()) {
+            return res.status(400).json({ error: "You are not allowed to delete this notification" });
+        }
+
+        await Notification.findByIdAndDelete(notificationId);
+        res.status(200).json({ message: "Notification deleted successfully" });
+    } catch (error) {
+        logger.error("Error in deleteNotification controller", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
 module.exports = {
-    getNotifications, deleteNotifications
+    getNotifications, deleteNotifications, deleteNotification
 };
