bfad945780bb876b1c03cb722d8cbd489e8074e8
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-06 05:54:34 +0300

    Get posts in homepage logic with react-query

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -3,6 +3,7 @@ import { BiRepost } from "react-icons/bi";
 import { FaRegBookmark } from "react-icons/fa6";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { avatarPlaceholder } from "../../assets/index";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
@@ -27,7 +28,7 @@ const Post = ({ post }) => {
 			<div className='flex gap-2 items-start p-4 border-b border-gray-700'>
 				<div className='avatar'>
 					<Link to={`/profile/${postOwner.username}`} className='w-8 rounded-full overflow-hidden'>
-						<img src={postOwner.profileImg || "/avatar-placeholder.png"} />
+						<img src={postOwner.profileImg || avatarPlaceholder} />
 					</Link>
 				</div>
 				<div className='flex flex-col flex-1'>
@@ -82,7 +83,7 @@ const Post = ({ post }) => {
 												<div className='avatar'>
 													<div className='w-8 rounded-full'>
 														<img
-															src={comment.user.profileImg || "/avatar-placeholder.png"}
+															src={comment.user.profileImg || avatarPlaceholder}
 														/>
 													</div>
 												</div>
