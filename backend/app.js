8407449f4b278e531583bef618dc4381a2d0fb82
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-06 13:08:22 +0300

    app ready for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require("path");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
@@ -8,10 +7,8 @@ const userRoutes = require("./routes/user.route");
 const postRoutes = require("./routes/post.route");
 const notificationRoutes = require("./routes/notification.route");
 const middleware = require("./utils/middleware");
-const config = require("./utils/config");
 const cloudinaryConfig = require("./cloudinary/cloudinary.config");
 
-const __dirname = path.resolve();
 
 cloudinaryConfig();
 
@@ -22,6 +19,7 @@ app.use(cors());
 app.use(middleware.requestLogger)
 app.disable("x-powered-by");
 
+
 app.get("/", (req, res) => {
     res.send("Backend Server is ready!");
 });
@@ -30,15 +28,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
 app.use(middleware.unknownEndPoint);
 app.use(middleware.errorHandler);
 
-if (config.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
-};
 
 module.exports = app;
