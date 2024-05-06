8d4e99b63601291773c95d55a68b9985fbe0aafb
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-05-06 12:30:00 +0300

    refactor: mutation modifed to use hook

--- frontend/src/pages/profile/EditProfileModal.jsx
+++ frontend/src/pages/profile/EditProfileModal.jsx
@@ -1,10 +1,8 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
+import useUpdateUserProfile from "../../hooks/useUpdateUserProfile";
 
 const EditProfileModal = ({ authUser }) => {
-	const queryClient = useQueryClient();
 
 	const [formData, setFormData] = useState({
 		fullName: "",
@@ -16,38 +14,7 @@ const EditProfileModal = ({ authUser }) => {
 		currentPassword: "",
 	});
 
-	const { mutate: updateProfile, isPending: isUpdatingProfile } = useMutation({
-		mutationFn: async () => {
-			try {
-				const res = await fetch(`/api/users/update`, {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json"
-					},
-					body: JSON.stringify(formData),
-				});
-
-				const data = await res.json();
-
-				if (!res.ok) throw new Error(data.error || "Something went wrong");
-
-				return data;
-
-			} catch (error) {
-				throw new Error(error.message);
-			}
-		},
-		onSuccess: () => {
-			toast.success("Profile details updated successfully");
-			Promise.all([
-				queryClient.invalidateQueries({ queryKey: "authUser" }),
-				queryClient.invalidateQueries({ queryKey: "userProfile" }),
-			]);
-		},
-		onError: (error) => {
-			toast.error(error.message);
-		}
-	})
+	const { updateProfile, isUpdatingProfile } = useUpdateUserProfile();
 
 	const handleInputChange = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -82,7 +49,7 @@ const EditProfileModal = ({ authUser }) => {
 						className='flex flex-col gap-4'
 						onSubmit={(e) => {
 							e.preventDefault();
-							updateProfile();
+							updateProfile(formData);
 						}}
 					>
 						<div className='flex flex-wrap gap-2'>
@@ -149,6 +116,7 @@ const EditProfileModal = ({ authUser }) => {
 						<button className='btn btn-primary rounded-full btn-sm text-white'>
 							{isUpdatingProfile ? <LoadingSpinner size="sm" /> : "Update"}
 						</button>
+						
 					</form>
 				</div>
 				<form method='dialog' className='modal-backdrop'>
